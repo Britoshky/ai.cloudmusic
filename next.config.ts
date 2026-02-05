0a889a74cc4d6e9ad204dfa3ecdc9c0f6f190855
Héctor Brito <britoshky@gmail.com>
2026-02-04 22:24:28 -0300

    corregido el tamaño del archivo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverActions: {
+    bodySizeLimit: '10mb', // Aumentar límite para archivos de audio
+  },
+  // Optimizar imágenes si las usas
+  images: {
+    domains: ['localhost', 'api-voz.cloudmusic.cl'],
+  },
 };
 
 export default nextConfig;
